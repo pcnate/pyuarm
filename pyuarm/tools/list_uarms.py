6a8c97a2900059c4ad780363fe942dfa5984476e
Nathan Baker <pcnate@gmail.com>
2017-06-09 21:13:32 -0500

    adding Arduino Mega hardware ID to allow flashing

--- pyuarm/tools/list_uarms.py
+++ pyuarm/tools/list_uarms.py
@@ -1,12 +1,15 @@
 from serial.tools import list_ports
 
 UARM_HWID_KEYWORD = "USB VID:PID=0403:6001"
+UARM_HWID_KEYWORD_ARDUINO = "USB VID:PID=2341:0042 LOCATION=1-4"
 
 
 def uarm_ports():
     uarm_ports = []
     for i in list_ports.comports():
         if i.hwid[0:len(UARM_HWID_KEYWORD)] == UARM_HWID_KEYWORD:
+            uarm_ports.append(i[0])
+        if i.hwid[0:len(UARM_HWID_KEYWORD_ARDUINO)] == UARM_HWID_KEYWORD_ARDUINO:
             uarm_ports.append(i[0])
     return uarm_ports
 
